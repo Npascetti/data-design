7634de49db3ce96c36e5fb30ef99defe49dbcc57
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-13 08:04:40 -0600

    added persona, user story, use case, conceptual model h1 & p tags

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 		<title>Nick Pascetti's Data Design Project</title>
 	</head>
 	<body>
-
+		<h1>Persona</h1>
+		<p></p>
+		<h1>User Story</h1>
+		<p></p>
+		<h1>Use Case/Interaction Flow</h1>
+		<p></p>
+		<h1>Conceptual Model</h1>
+		<p></p>
 	</body>
 </html>
