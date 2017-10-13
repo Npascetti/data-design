c5c825cb6808332c25ee6f78b348e368a28c66d1
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-13 09:08:25 -0600

    added list items for persona section

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
 	</head>
 	<body>
 		<h1>Persona</h1>
-		<p></p>
+			<ul>
+					<li>Name: Karissa</li>
+					<li>Age: 25</li>
+					<li>Education: BA Anthropology</li>
+					<li>Occupation: Barista</li>
+					<li>Technology: MacBook Air, iPhone 8</li>
+					<li>Hobbies: Cycling, local music,</li>
+					<li></li>
+			</ul>
+			<p>Karissa is a recent transplant to Albuquerque from ..., and is looking to meet new people and find fun things to do locally.</p>
 		<h1>User Story</h1>
 		<p></p>
 		<h1>Use Case/Interaction Flow</h1>
