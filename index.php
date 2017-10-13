d331415acffa08f318044fb07d1697a8ae2cdf54
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-13 09:48:07 -0600

    added more description to persona section

--- index.php
+++ index.php
@@ -13,9 +13,15 @@
 					<li>Occupation: Barista</li>
 					<li>Technology: MacBook Air, iPhone 8</li>
 					<li>Hobbies: Cycling, local music,</li>
-					<li></li>
+					<li>Relationship Status: Single</li>
 			</ul>
-			<p>Karissa is a recent transplant to Albuquerque from ..., and is looking to meet new people and find fun things to do locally.</p>
+			<p>Karissa is a recent transplant to Albuquerque from ..., and is looking to meet new people and find fun things to do locally.
+				She is currently working as a barista at a local coffee shop, but is looking for employment in her field. She moved to Albuquerque
+				after college mainly to get a "new perspective" and because she "needed a change of scenery." She is always tweeting quippy observations
+				on her phone, noting the idiosyncracies of Albuquerque compared with the midwest where she hails from.</p>
+			<p>Karissa likes the people that she works with, but wants to branch out and meet new friends to be able to go see a concert with,
+				have a beer, or find new fun local things to do. She is a registered user of Reddit, but mainly uses Reddit for entertainment,
+				viewing funny posts and memes, or finding local events and happenings. She is mainly a passive user of Reddit, rarely posting herself, 				instead preferring to use her Twitter account for her personal musings </p>
 		<h1>User Story</h1>
 		<p></p>
 		<h1>Use Case/Interaction Flow</h1>
