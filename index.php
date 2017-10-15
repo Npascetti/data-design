0df571c2938859b28e9897d3c0623cab9868b8f5
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-15 00:07:15 -0600

    added header and nav elements with links to reddit post and github repository added css styling to nav

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
 	</head>
 	<header>
 		<h1>Nick Pascetti's Data Design Project</h1>
+		<nav>
+			<ul>
+			<li><a href="https://www.reddit.com/r/Albuquerque/comments/75q2kp/hows_verizon_tmobile_celldata_coverage_aibf_field/">Example Reddit Post</a></li>
+			<li><a href="https://github.com/Npascetti/data-design">Github Data Design Repository</a></li>
+			</ul>
+		</nav>
 	</header>
 	<body>
 		<h1>Persona</h1>
@@ -25,17 +31,17 @@
 				on her phone, noting the idiosyncracies of Albuquerque compared with the midwest where she hails from.</p>
 			<p>Karissa likes the people that she works with, but wants to branch out and meet new friends to be able to go see a concert with,
 				have a beer, or find new fun local things to do. She is a registered user of Reddit, but mainly uses Reddit for entertainment,
-				viewing funny posts and memes, or finding local events and happenings. She is primarily passive user, rarely posting herself, 				instead preferring to use her Twitter account for her personal musings. She does, however, ask questions in posts and comment threads when 				she has a specific question regarding an event she is interested in, i.e., should I arrive early to Geeks Who Drink to make sure I get a 				seat, or is it usually not too crowded?</p>
+				viewing funny posts and memes, or finding local events and happenings. She is primarily passive user, rarely posting herself, instead preferring to use her Twitter 				account for her personal musings. She does, however, ask questions in posts and comment threads when she has a specific question regarding an event she is 				interested in, i.e., should I arrive early to Geeks Who Drink to make sure I get a seat, or is it usually not too crowded?</p>
 		<h1>User Story</h1>
 		<p>As a registered user, Karissa wants to be able to respond to posts/comment threads about events, to ask questions about the event.</p>
 		<h1>Use Case/Interaction Flow</h1>
 			<ul>
 				<li>Karissa clicks on the post about an event that she is interested in.</li>
 				<li>Reddit loads the page with the post, displaying the full content of the post, and any comment threads.</li>
-				<li>Karissa clicks on the comment text area to fill out the comment field with her question. If she is responding to a particular comment  				in a thread, she clicks the reply link beneath the respective comment.</li>
+				<li>Karissa clicks on the comment text area to fill out the comment field with her question. If she is responding to a particular comment in a thread, she clicks 				the reply link beneath the respective comment.</li>
 				<li>If she is replying to a comment, the site opens a comment field when reply button is clicked.</li>
 				<li>Karissa fills out the comment field with her question. She clicks the save button to post her comment/question.</li>
-				<li>The site then displays her comment in its own thread beneath the original post if it was responding directly to the original post. If her comment was repsonding to another comment thread, then her comment displays beneath the comment she was responding to.</li>
+				<li>The site then displays her comment in its own thread beneath the original post if it was responding directly to the original post. If her comment was repsonding 				to another comment thread, then her comment displays beneath the comment she was responding to.</li>
 			</ul>
 		<h1>Conceptual Model</h1>
 			<h2>Entities & Attributes</h2>
