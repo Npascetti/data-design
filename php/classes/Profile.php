44154c6ca2b6fb19011c24f3d420f505ff28536c
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-23 11:10:22 -0600

    finished class creation for Profile, began constructor

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -20,6 +20,27 @@ class Profile {
 	/**
 	 * a link to the avatar image of the profile
 	 * @var string $profileAvatar
+	 **/
+	private $profileAvatar;
+	/**
+	 * hash for the profile password
+	 * @var  string $profileHash
+	 **/
+	private $profileHash;
+	/**
+	 * salt for the profile password
+	 * @var string $profileSalt
+	 **/
+	private $profileSalt;
+
+	/**
+	 * constructor for this Profile
+	 *
+	 * @param string | Uuid $newProfileId id of this Profile or null if a new profile
+	 * @param string $newProfileUserName string containing the profile username
+	 * @param string $newProfileAvatar string containing link to profile avatar image or null if unused
+	 * @param string $newProfileHash string containing the profile password hash
+	 * @param string $newProfileSalt string containing the profile password salt
 	 */
 }
 ?>
