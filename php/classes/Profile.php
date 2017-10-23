6bd0dedd4f6bcecaa300cf00a4ba9f0b5137374e
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-23 09:27:36 -0600

    began writing Profile Class

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,6 +1,25 @@
 <?php
-
 /**
-*
-*/
+ * A class representing a profile on Reddit
+ *
+ * @author Nick Pascetti <Npascetti@gmail.com>
+ * @version 1.0.0
+ **/
+
+class Profile {
+	/**
+	 * id for the user Profile; this is the primary key
+	 * @var Uuid $profileId
+	 **/
+	private $profileId;
+	/**
+	 * the username of the profile
+	 * @var string $profileUserName
+	 **/
+	private $profileUserName;
+	/**
+	 * a link to the avatar image of the profile
+	 * @var string $profileAvatar
+	 */
+}
 ?>
